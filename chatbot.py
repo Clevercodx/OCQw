2149584141f99deb511ba5c9f2cf83f39f1c946a
Clevercodex <115954304+Clevercodx@users.noreply.github.com>
2023-09-06 20:29:52 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import requests
+import json
+
+# Définissez votre clé API Bard
+API_KEY = "Votre clé API Bard"
+
+# Définissez la fonction qui interroge l'API Bard
+def get_response(query):
+    url = "https://bard.ai/v1/query"
+    payload = {"query": query, "api_key": API_KEY}
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        return json.loads(response.content)["response"]
+    else:
+        return None
+
+# Définissez la fonction qui gère l'interface utilisateur
+def main():
+    st.title("Chatbot Bard")
+    query = st.text_input("Entrez votre question ou votre requête")
+    response = get_response(query)
+    if response is not None:
+        st.write(response)
+
+# Lancez l'application
+if __name__ == "__main__":
+    main()
 
